2c0e6dc83a654f8a7184ae1043be2359d80f4a84
AndreCalder <andre.calderonbarbeito@gmail.com>
2024-11-27 11:40:40 -0600

    Release

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,78 @@
+"use client";
+
 import Image from "next/image";
+import moment from "moment";
+import React, { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  var now = moment(new Date());
+  var end = moment("2025-01-15");
+
+  const [dateDiff, setDateDiff] = React.useState({days: "-", hours: "-", minutes: "-", seconds: "-"});
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      let duration = moment.duration(end.diff(moment()));
+
+      const days = Math.floor(duration.asDays()).toString();
+      const hours = duration.hours().toString();
+      const minutes = duration.minutes().toString();
+      const seconds = duration.seconds().toString();
+
+      setDateDiff({days, hours, minutes, seconds});
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div className="py-5">
+      <nav className="nav-container">
+        <div className="navbar py-2">
+          <a className="navbar__logo"><img src="img/logo.png" alt="M4 Logo" /></a>
         </div>
+      </nav>
+      <main>
+        <section className="header">
+          <div className="header__flex-container">
+            <div className="header__content">
+              <div className="header__content__top">
+                <div className="header__content__top__img">
+                  <figure><img src="img/isometric.png" alt="" /></figure>
+                </div>
+                <div className="header__content__top__countdown-container" id="clockdiv">
+                  <div className="header__content__top__countdown-container__time-container">
+                    <div className="days">{dateDiff.days}</div>
+                    <div className="days-label">Dias</div>
+                  </div>
+                  <div className="header__content__top__countdown-container__time-container">
+                    <div className="hours">{dateDiff.hours}</div>
+                    <div className="hours-label">Horas</div>
+                  </div>
+                  <div className="header__content__top__countdown-container__time-container">
+                    <div className="minutes">{dateDiff.minutes}</div>
+                    <div className="minutes-label">Minutos</div>
+                  </div>
+                  <div className="header__content__top__countdown-container__time-container">
+                    <div className="seconds">{dateDiff.seconds}</div>
+                    <div className="seconds-label">Segundos</div>
+                  </div>
+                </div>
+              </div>
+              <div className="header__content__bottom">
+                <div>
+                  <p>MiLegalista esta en Silicon Valley construyendo la mejor experiencia</p>
+                </div>
+                <div className="header__content__bottom__contact-info">
+                  <a href="https://www.instagram.com/milegalista/"><img src="/img/instagram-icon.png" alt="" /></a>
+                  <p>info@milegalista.com</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
