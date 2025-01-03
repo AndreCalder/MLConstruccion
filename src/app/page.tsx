3a2e047b363fbc15c9e014eb0da9889c4f3843d1
AndreCalder <andre.calderonbarbeito@gmail.com>
2025-01-02 19:09:57 -0600

    Changed date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import React, { useEffect } from "react";
 export default function Home() {
 
   var now = moment(new Date());
-  var end = moment("2024-12-30");
+  var end = moment("2025-01-10");
 
   const [dateDiff, setDateDiff] = React.useState({days: "-", hours: "-", minutes: "-", seconds: "-"});
 
